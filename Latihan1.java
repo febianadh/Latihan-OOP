dac64291824b82e5dab26793b70f14ba91d1ed44
febianadh <96273984+febianadh@users.noreply.github.com>
2022-04-07 21:06:52 +0700

    Add files via upload

---
+++ Latihan1.java
@@ -0,0 +1,23 @@
+
+package latihan1;
+
+public class Latihan1 {
+    String merk;
+    String type;
+    String warna;
+    String harga;
+    String tahun_rilis;
+    
+    public static void main(String[] args) {
+        
+        handphone objek = new handphone();
+        System.out.println(objek.merk = "Merk : " + "iphone");
+        System.out.println(objek.type = "Type : " + "13 Pro Max");
+        System.out.println(objek.warna = "Warna : " + "Sierra Blue");
+        System.out.println(objek.harga = "Harga : " + 20000000);
+        System.out.println(objek.tahun_rilis = "Tahun_Rilis : " + 2021);
+       
+        
+    }
+    
+}
